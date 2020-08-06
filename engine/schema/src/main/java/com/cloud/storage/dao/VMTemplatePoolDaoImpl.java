d9d7c1279fce90a608b9d22ab5defaa73e9dcf62
nvazquez <nicovazquez90@gmail.com>
2020-08-06 16:12:32 -0300

    Fix index out of bounds exception

--- engine/schema/src/main/java/com/cloud/storage/dao/VMTemplatePoolDaoImpl.java
+++ engine/schema/src/main/java/com/cloud/storage/dao/VMTemplatePoolDaoImpl.java
@@ -264,7 +264,7 @@ public class VMTemplatePoolDaoImpl extends GenericDaoBase<VMTemplateStoragePoolV
     @Override
     public VMTemplateStoragePoolVO findByHostTemplate(Long hostId, Long templateId) {
         List<VMTemplateStoragePoolVO> result = listByHostTemplate(hostId, templateId);
-        return (result.size() == 0) ? null : result.get(1);
+        return (result.size() == 0) ? null : result.get(0);
     }
 
     @Override
